85afc67b60fba18e1bdc64efb8376bab596c95f2
Lidia Bonifert <bonifert.lidia@gmail.com>
2018-03-28 16:29:27 +0200

    crossorigin tag for magical endpoint

--- src/main/java/com/codecool/shitwish/product/repository/ProductRepository.java
+++ src/main/java/com/codecool/shitwish/product/repository/ProductRepository.java
@@ -4,12 +4,14 @@ import com.codecool.shitwish.product.model.Product;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "product", path = "products")
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
+    @CrossOrigin
     List<Product> findByActive(@Param("status") Boolean status);
 
     Product findById(@Param("id") Long id);
