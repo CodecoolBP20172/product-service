9af03079c90b35a6cce5e2504734b6b7e74e716e
Lidia Bonifert <bonifert.lidia@gmail.com>
2018-03-28 14:38:33 +0200

    add hotfix

--- src/main/java/com/codecool/shitwish/product/controller/restController.java
+++ src/main/java/com/codecool/shitwish/product/controller/restController.java
@@ -2,7 +2,9 @@ package com.codecool.shitwish.product.controller;
 
 import com.codecool.shitwish.product.model.Product;
 import com.codecool.shitwish.product.repository.ProductRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +15,11 @@ public class restController {
     ProductRepository productRepository;
 
     @PostMapping("/new-product")
-    public Product createProduct(@ModelAttribute Product product) {
-        product.setActive(true);
-        return productRepository.save(product);
+    public HttpStatus createProduct(@RequestBody JSONObject req) {
+        System.out.println(req);
+//        product.setActive(true);
+        return HttpStatus.OK;
+//                productRepository.save(product);
     }
 
     @PostMapping("/sell-product")
@@ -27,4 +31,11 @@ public class restController {
         }
         return product;
     }
+
+//    @GetMapping("/products/{status}")
+//    public ResponseEntity getUserforProduct(@PathVariable("status") boolean status) {
+//        List<Product> productList = productRepository.findByActive(status);
+//        productList.forEach(p -> p.setUsername(productService.getUsernameById(p.getUserId())));
+//
+//    }
 }
